86bc1549cf7b328fd336b0b12b862fa72d62ec86
Clay Smith <uhclay@gmail.com>
2020-11-16 16:43:18 -0800

    Add stub for queue route

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import OfficerController from '../controllers/officerController';
+import QueueController from '../controllers/queueController';
 import SearchController from '../controllers/searchController';
 const routes = Router();
 routes.get('/', OfficerController.list);
 routes.get('/search', SearchController.search);
+routes.get('/queue', QueueController.get);
 routes.get('/:id', OfficerController.get);
 export default routes;
