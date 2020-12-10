ec56096d801511db8ba5cafad2527a4f654ae27b
Clay Smith <clay.smith@rallyhealth.com>
2020-12-09 17:44:53 -0800

    Add content-type header for POST /api/queue

--- controllers/queueController.js
+++ controllers/queueController.js
@@ -7,13 +7,14 @@ class QueueController {
   }
 
   get(req, res) {
-    return axios.get(`${this.baseUrl}/api/queue`, {headers: {'Content-Type': 'application/json'}})
+    return axios.get(`${this.baseUrl}/api/queue`)
       .then(request => res.status(200).json(request.data));
   }
 
   enqueue(req, res) {
     const data = req.data;
-    return axios.post(`${this.baseUrl}/api/queue`, typeof data === 'string' ? JSON.parse(data) : data)
+    return axios.post(`${this.baseUrl}/api/queue`, typeof data === 'string' ? JSON.parse(data) : data, 
+      {headers: {'Content-Type': 'application/json'}})
       .then(request => res.status(200).json(request.data));
   }
 }
