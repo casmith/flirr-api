815cb2782aff95e39675db8b313672d737d163c5
Clay Smith <uhclay@gmail.com>
2020-12-13 14:39:39 -0800

    Add history controller

---
+++ controllers/historyController.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+class QueueController {
+
+  constructor(baseUrl) {
+    this.baseUrl = baseUrl;
+  }
+
+  get(req, res) {
+    return axios.get(`${this.baseUrl}/api/history`)
+      .then(request => res.status(200).json(request.data));
+  }
+}
+export default QueueController;
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import HistoryController from '../controllers/historyController';
 import QueueController from '../controllers/queueController';
 import SearchController from '../controllers/searchController';
 import ServerController from '../controllers/serverController';
@@ -12,15 +13,17 @@ const listsDirectory = process.env.LISTS_DIR || 'lists';
 const elasticSearchBaseUrl = process.env.ES_BASE_URL || 'http://localhost:9200';
 const elasticSearchIndex = process.env.ES_INDEX || 'downloads';
 
+const historyController = new HistoryController(marvinBaseUrl);
 const queueController = new QueueController(marvinBaseUrl);
 const searchController = new SearchController(elasticSearchBaseUrl, elasticSearchIndex, listsDirectory);
 const serverController = new ServerController(marvinBaseUrl);
 const statusController = new StatusController(marvinBaseUrl);
 
-routes.get('/search', (req, res) => searchController.search(req, res));
+routes.get('/history', (req, res) => historyController.get(req, res));
 routes.get('/queue', (req, res) => queueController.get(req, res));
 routes.post('/queue', (req, res) => queueController.enqueue(req, res));
-routes.get('/status', (req, res) => statusController.get(req, res));
+routes.get('/search', (req, res) => searchController.search(req, res));
 routes.get('/server/users', (req, res) => serverController.listUsers(req, res));
+routes.get('/status', (req, res) => statusController.get(req, res));
 
 export default routes;
