adb77e519d68091f1d74a3ba9bf89d9714421791
Clay Smith <casmith@users.noreply.github.com>
2021-05-20 18:36:30 -0700

    Add websocket listener for updating queue status

--- server.js
+++ server.js
@@ -1,17 +1,71 @@
-import express from 'express';
+import amqp from 'amqplib/callback_api';
 import bodyParser from 'body-parser';
+import EventEmitter from 'events';
+import express from 'express';
+import socketIo from 'socket.io';
 import routes from './routes/index.js';
 
+const emitter = new EventEmitter();
+
 function Server() {
-    const app = express();
     const port = process.env.PORT || 8000;
+
+    const app = express();
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
-    app.use('/', routes);
-    app.listen(port, () => {
-        console.log(`Server started on port ${port}`);
-    });
+    app.use('/api', routes);
+
+    const server = require('http').Server(app);
+
+    setupRabbitMqClient(process.env.RABBIT_MQ_HOST);
+    setupWebsocketListener(server);
+
+    server.listen(port);
+
     return app;
+}
+
+const setupWebsocketListener = (server) => {
+    const io = socketIo(server);
+    io.on('connection', function(socket) {
+        console.log('user connected');
+        emitter.on('queues', data => {
+            socket.emit('queue-updated', { data });
+        });
+    });
+}
+
+const setupRabbitMqClient = (rabbitMqHost) => {
+    amqp.connect(`amqp://${rabbitMqHost}`, (error0, connection) => {
+        if (error0) {
+            throw error0;
+        }
+
+        let onQueueUpdate;
+
+        connection.createChannel((error1, channel) => {
+            if (error1) {
+                throw error1;
+            }
+
+            const exchange = 'queue-updated'
+            channel.assertExchange(exchange, 'fanout', { durable: false });
+
+            channel.assertQueue('', {
+              exclusive: true
+            }, (error2, q) => {
+                if (error2) {
+                    throw error2;
+                }
+                channel.bindQueue(q.queue, exchange, '');
+                channel.consume(q.queue, msg => {
+                    if (msg.content) {
+                        emitter.emit('queues', JSON.parse(msg.content.toString()));
+                    }
+                }, { noAck: true });
+            });
+        });
+    });
 }
 
 export default Server;
